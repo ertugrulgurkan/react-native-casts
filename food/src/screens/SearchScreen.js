5569c34a7b7dbb246028b50aa1f007ee7132d1b9
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-11-17 20:21:14 +0300

    food - search page was created.

---
+++ food/src/screens/SearchScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+const SearchScreen = () => {
+    return (
+        <View>
+            <Text>Search Screen</Text>
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
