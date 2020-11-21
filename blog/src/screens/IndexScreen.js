496179fb747d1dbc079a373756d0f09734f1efc5
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-11-21 20:02:00 +0300

    new context init.

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
+const value = useContext(BlogContext);
+
   return (
     <View>
       <Text>Index Screen</Text>
