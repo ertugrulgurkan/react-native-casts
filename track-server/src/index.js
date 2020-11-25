df44ed65f31d95b3308dd9e196a4a4e395082362
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-11-26 00:58:27 +0300

    authRoutes.js created index .js changed

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,11 +1,28 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-app.get('/' , (req, res)=> {
-    res.send('Hi There');
+const mongoUri =
+  "mongodb+srv://admin:admin@cluster0.sjprc.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
 });
 
-app.listen(3000, ()=> {
-    console.log("Listening on port 3000");
-})
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to mongo", err);
+});
+app.get("/", (req, res) => {
+  res.send("Hi There");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
