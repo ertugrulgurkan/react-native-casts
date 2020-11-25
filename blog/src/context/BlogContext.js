7b9bf6e8ae3b4233989e20b66bcca47bea5581c9
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-11-25 21:55:49 +0300

    Blog - Post Update Delete from JsonApi

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -11,15 +11,6 @@ const blogReducer = (state, action) => {
       });
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
-    case "add_blogpost":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content,
-        },
-      ];
     default:
       return state;
   }
@@ -34,20 +25,21 @@ const getBlogPosts = (dispatch) => {
 
 const addBlogPost = (dispatch) => {
   return async (title, content, callback) => {
-    await jsonServer.post("/blogposts", {title, content})
-    //dispatch({ type: "add_blogpost", payload: { title, content } });
+    await jsonServer.post("/blogposts", { title, content });
     if (callback) {
       callback();
     }
   };
 };
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: "delete_blogpost", payload: id });
   };
 };
 const editBlogPost = (dispatch) => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
     dispatch({
       type: "edit_blogpost",
       payload: { id, title, content },
