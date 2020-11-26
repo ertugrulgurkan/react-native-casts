a997c62f4ec4a092f78b06c80a33f051840c5b60
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-11-26 17:51:31 +0300

    track route added

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,7 +1,11 @@
 require("./models/User");
+require("./models/Track");
+
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
+
 const bodyParser = require("body-parser");
 const requireAuth = require("./middlewares/requireAuth");
 
@@ -9,6 +13,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri =
   "mongodb+srv://admin:admin@cluster0.sjprc.mongodb.net/test?retryWrites=true&w=majority";
