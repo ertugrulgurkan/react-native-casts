17ad6bf86c060131f767d36a692f6c84008d82ee
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-11-27 14:12:37 +0300

    authContext necessary funcs addded.

--- tracks/App.js
+++ tracks/App.js
@@ -8,6 +8,7 @@ import SignupScreen from "./src/screens/SignupScreen";
 import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
+import { Provider as AuthProvider } from "./src/context/AuthContext";
 
 const switchNavigator = createSwitchNavigator({
   loginFlow: createStackNavigator({
@@ -24,5 +25,12 @@ const switchNavigator = createSwitchNavigator({
   }),
 });
 
+const App = createAppContainer(switchNavigator);
 
-export default createAppContainer(switchNavigator);
+export default () => {
+  return (
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  );
+};
