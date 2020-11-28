0a7310b37a63cb021e5bdb5b99b68ade61bb5afd
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-11-29 00:46:44 +0300

    clear error message when screens changed

--- tracks/src/screens/SigninScreen.js
+++ tracks/src/screens/SigninScreen.js
@@ -3,12 +3,14 @@ import { View, StyleSheet } from "react-native";
 import { Context as AuthContext } from "../context/AuthContext";
 import AuthForm from "../components/AuthForm";
 import NavLink from "../components/NavLink";
+import { NavigationEvents } from "react-navigation";
 
 const SigninScreen = () => {
-  const { state, signin } = useContext(AuthContext);
+  const { state, signin, clearErrorMessage } = useContext(AuthContext);
 
   return (
     <View style={styles.container}>
+      <NavigationEvents onWillFocus={clearErrorMessage} />
       <AuthForm
         headerText="Sign in for Tracker"
         errorMessage={state.errorMessage}
@@ -16,11 +18,7 @@ const SigninScreen = () => {
         onSubmit={signin}
       />
 
-      <NavLink
-        routeName="Signup"
-        text="Doesnt have an account? Sign Up!"
-      />
-
+      <NavLink routeName="Signup" text="Doesnt have an account? Sign Up!" />
     </View>
   );
 };
--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -3,12 +3,13 @@ import { View, StyleSheet } from "react-native";
 import { Context as AuthContext } from "../context/AuthContext";
 import AuthForm from "../components/AuthForm";
 import NavLink from "../components/NavLink";
-
+import { NavigationEvents } from "react-navigation";
 const SignupScreen = () => {
-  const { state, signup } = useContext(AuthContext);
+  const { state, signup, clearErrorMessage } = useContext(AuthContext);
 
   return (
     <View style={styles.container}>
+      <NavigationEvents onWillFocus={clearErrorMessage} />
       <AuthForm
         headerText="Sign Up for Tracker"
         errorMessage={state.errorMessage}
@@ -20,7 +21,6 @@ const SignupScreen = () => {
         routeName="Signin"
         text="Already have an account? Sign in instead."
       />
-
     </View>
   );
 };
