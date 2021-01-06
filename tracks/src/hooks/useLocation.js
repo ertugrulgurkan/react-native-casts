dbb4d81ae5887811cf51fa42d2d9875d94f879be
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2021-01-06 19:04:58 +0300

    useCallBack Hook

--- tracks/src/hooks/useLocation.js
+++ tracks/src/hooks/useLocation.js
@@ -7,37 +7,40 @@ import {
 
 export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
-  const [subscriber, setSubscriber] = useState(null);
-
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestPermissionsAsync();
-      if (!granted) {
-        throw new Error("Location permission not granted");
-      }
-      const sub = await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10,
-        },
-        callback
-      );
-      setSubscriber(sub);
-    } catch (e) {
-      setErr(e);
-    }
-  };
 
   useEffect(() => {
-      if(shouldTrack){
-        startWatching();
+    let subscriber;
+    const startWatching = async () => {
+      try {
+        const { granted } = await requestPermissionsAsync();
+        if (!granted) {
+          throw new Error("Location permission not granted");
+        }
+        subscriber = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10,
+          },
+          callback
+        );
+      } catch (e) {
+        setErr(e);
       }
-      else{
-        subscriber.remove();  
-        setSubscriber(null);
+    };
+    if (shouldTrack) {
+      startWatching();
+    } else {
+      if (subscriber) {
+        subscriber.remove();
       }
-    
+      subscriber = null;
+    }
+    return () => {
+      if (subscriber) {
+        subscriber.remove();
+      }
+    };
   }, [shouldTrack, callback]);
 
   return [err];
